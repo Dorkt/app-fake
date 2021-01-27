3f1080fff97fe19f4ce5612b34336409a8c63d4b
Dorkt <gabrieldorkt@hotmail.com>
2021-01-27 11:51:57 -0300

    created types from user

---
+++ src/store/ducks/user/types.ts
@@ -0,0 +1,53 @@
+/* Action types */
+import User from '../../application/models/users/user'
+import { IPaginator, IComponentState } from '../root.types'
+
+export enum UserActionTypes {
+    RESET_CREATE_USER = '@users/RESET_CREATE_USER',
+
+    CHANGE_USER = '@users/CHANGE_USER',
+    CHANGE_PAGINATOR = '@users/CHANGE_PAGINATOR',
+    CHANGE_SEARCH_PAGINATOR = '@users/CHANGE_SEARCH_PAGINATOR',
+    CHANGE_REMOVE_DIALOG = '@users/CHANGE_REMOVE_DIALOG',
+    
+    CREATE_REQUEST = '@users/CREATE_REQUEST',
+    CREATE_SUCESS = '@users/CREATE_SUCESS',
+    CREATE_FAILURE = '@users/CREATE_FAILURE',
+
+    FIND_REQUEST = '@users/FIND_REQUEST',
+    FIND_SUCESS = '@users/FIND_SUCESS',
+    FIND_FAILURE = '@users/CREATE_FAILURE',
+
+    LOAD_USERS_REQUEST = '@users/LOAD_USERS_REQUEST',
+    LOAD_USERS_SUCESS = '@users/LOAD_USERS_SUCESS',
+    LOAD_USERS_FAILURE = '@users/LOAD_USERS_FAILURE',
+
+    UPDATE_REQUEST = '@users/UPDATE_REQUEST',
+    UPDATE_SUCESS = '@users/UPDATE_SUCESS',
+    UPDATE_FAILURE = '@users/UPDATE_FAILURE',
+
+    REMOVE_REQUEST = '@users/REMOVE_REQUEST',
+    REMOVE_SUCESS = '@users/REMOVE_SUCESS',
+    REMOVE_FAILURE = '@users/REMOVE_FAILURE'
+}
+
+interface ICreateState extends IComponentState {
+    readonly user: User
+    readonly data: ErrorEvent
+}
+
+interface IRemoveState extends IComponentState {
+    readonly visibilityModal: boolean
+    readonly userIdForRemove: string
+}
+
+interface IListUserState extends IComponentState {
+    readonly users: User[]
+    readonly paginator: IPaginator
+}
+
+export interface IUserState {
+    readonly createUser: ICreateState,
+    readonly removeUser: IRemoveState,
+    readonly listUsers: IListUserState,
+}
